52f8ea393a312e5fc128a699f1f966a2174326b6
Esquivel28 <109557909+Esquivel28@users.noreply.github.com>
2023-07-18 12:15:52 -0600

    Started OverController

---
+++ src/OvertureLib/MotorControllers/ControllerNeutralMode/ControllerNeutralMode.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+/**
+ * @brief The neutral mode determines how the motor controller behaves when not receiving any singnal.
+ **/
+enum class ControllerNeutralMode {
+    Coast, Brake
+};
+
