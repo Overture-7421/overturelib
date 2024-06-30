84d3bc54a7427a45354c36b70b4aa76c0c45ceeb
Vanessa_Carranza <v.carranza.carri@gmail.com>
2024-06-29 23:09:15 -0600

    OvertureController changes

--- Gamepad/OvertureController/OvertureController.cpp
+++ Gamepad/OvertureController/OvertureController.cpp
@@ -13,5 +13,124 @@ OvertureController::OvertureController(int port, bool isXbox){
 };
 
 frc2::Trigger OvertureController::a(){
-        return (isXbox = true) ? xboxController->GetAButton : ps5Controller->GetCrossButton;
-        };
+        return (isXbox == true) ? xboxController->A() : ps5Controller->Cross();
+};
+
+frc2::Trigger OvertureController::b(){
+        return (isXbox == true) ? xboxController->B() : ps5Controller->Circle();
+};
+
+frc2::Trigger OvertureController::x(){
+        return (isXbox == true) ? xboxController->X() : ps5Controller->Square();
+};
+
+frc2::Trigger OvertureController::y(){
+        return (isXbox == true) ? xboxController->Y() : ps5Controller->Triangle();
+};
+
+frc2::Trigger OvertureController::leftBumper(){
+        return (isXbox == true) ? xboxController->LeftBumper() : ps5Controller->L1();
+};
+
+frc2::Trigger OvertureController::rightBumper(){
+        return (isXbox == true) ? xboxController->RightBumper() : ps5Controller->R1();
+};
+
+/*
+Necesitan new trigger
+
+
+frc2::Trigger OvertureController::leftTrigger(double threshold){
+        return (isXbox == true) ? xboxController->LeftTrigger(threshold) :
+
+frc::Trigger OvertureController::leftTrigger(double threshold){
+        return (isXbox == true) ? xboxController->LeftTrigger(threshold) :
+}
+}*/
+
+frc2::Trigger OvertureController::leftStick(){
+        return (isXbox == true) ? xboxController->LeftStick() : ps5Controller->L3();
+};
+
+frc2::Trigger OvertureController::rightStick(){
+        return (isXbox == true) ? xboxController->RightStick() : ps5Controller->R3();
+}
+
+frc2::Trigger OvertureController::start(){
+        return (isXbox == true) ? xboxController->Start() : ps5Controller->Options();
+};
+
+/*
+ps5Controller Create() necesita parámetros
+
+frc2::Trigger OvertureController::select(){
+        return (isXbox == true) ? xboxController->Back() : ps5Controller->Create());
+};
+*/
+
+/*
+Necesita un new Trigger
+frc2::Trigger OvertureController::trackpad() {
+        return (isXbox == true) ? new... : ps5Controller.Touchpad();
+
+*/
+
+frc2::Trigger OvertureController::upDpad() {
+        return abstractedController->POVUp();
+    }
+
+ frc2::Trigger OvertureController::downDpad() {
+        return abstractedController->POVDown();
+    }
+
+frc2::Trigger OvertureController::leftDpad() {
+        return abstractedController->POVLeft();
+    }
+
+frc2::Trigger OvertureController::rightDpad() {
+        return abstractedController->POVRight();
+    }
+
+double OvertureController::getRightTriggerAxis() {
+        return (isXbox == true) ? xboxController->GetRightTriggerAxis()
+                : units::math::min(units::math::abs(ps5Controller->GetR2Axis()), 0.99);
+    }
+
+double OvertureController::getRightTriggerAxis() {
+        return (isXbox == true) ? xboxController->GetRightTriggerAxis()
+                : units::math::min(units::math::abs(ps5Controller->GetL2Axis()), 0.99);
+    }
+
+double OvertureController::getLeftX() {
+        return (isXbox == true) ? xboxController->GetLeftX() : ps5Controller->GetLeftX();
+    }
+
+double OvertureController::getLeftY() {
+        return (isXbox == true) ? xboxController->GetLeftY() : ps5Controller->GetLeftY();
+    }
+
+double OvertureController::getRightX() {
+        return (isXbox == true) ? xboxController->GetRightX() : ps5Controller->GetRightX();
+    }
+
+double OvertureController::getRightY() {
+        return (isXbox == true) ? xboxController->GetRightY() : ps5Controller->GetRightY();
+    }
+
+/* 
+No hay method llamado GetHID
+
+frc2::CommandGenericHID OvertureController::getHID() {
+        return (isXbox == true) ? xboxController->() : ps5Controller->();
+    } 
+
+frc2::CommandGenericHID OvertureController::getHID() {
+        return (isXbox == true) ? xboxController->() : ps5Controller->();
+    }
+
+*/
+
+void OvertureController::rumbleController(double leftIntensity, double rightIntensity) {
+        getRumbleHID().SetRumble(frc::GenericHID::RumbleType::kLeftRumble, leftIntensity);
+        getRumbleHID().SetRumble(frc::GenericHID::RumbleType::kRightRumble, rightIntensity);
+    }
--- Gamepad/OvertureController/OvertureController.h
+++ Gamepad/OvertureController/OvertureController.h
@@ -6,6 +6,7 @@
 #include <frc2/command/button/CommandPS5Controller.h>
 #include <frc2/command/button/CommandGenericHID.h>
 #include <frc2/command/button/Trigger.h>
+#include <math/Utils.h>
 
 #pragma once
 
