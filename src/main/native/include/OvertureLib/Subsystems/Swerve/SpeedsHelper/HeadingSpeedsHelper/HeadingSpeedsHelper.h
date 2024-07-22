cdb11a7f0f693e1886bb83b2bcdca510854873ab
Luis Leyva <luisoleyvan@gmail.com>
2024-07-22 16:33:23 -0600

    Apply Spotless and Fix Create Release

--- src/main/native/include/OvertureLib/Subsystems/Swerve/SpeedsHelper/HeadingSpeedsHelper/HeadingSpeedsHelper.h
+++ src/main/native/include/OvertureLib/Subsystems/Swerve/SpeedsHelper/HeadingSpeedsHelper/HeadingSpeedsHelper.h
@@ -12,12 +12,14 @@
 // Class for controlling the rotation of the robot
 class HeadingSpeedsHelper: public SpeedsHelper {
 public:
-	HeadingSpeedsHelper(frc::ProfiledPIDController<units::radian> headingController, SwerveChassis* chassis);
-	void setTargetAngle(units::radian_t targetAngle);	
+	HeadingSpeedsHelper(
+			frc::ProfiledPIDController<units::radian> headingController,
+			SwerveChassis *chassis);
+	void setTargetAngle(units::radian_t targetAngle);
 	void alterSpeed(frc::ChassisSpeeds &inputSpeed) override;
 
 private:
 	frc::ProfiledPIDController<units::radian> headingController;
-	SwerveChassis* chassis = nullptr;
+	SwerveChassis *chassis = nullptr;
 	units::radian_t targetAngle;
 };
