0b925930d46f983f5e03c05837a028b7a1cc5494
EricBrwn <a01572515@tec.mx>
2025-07-31 16:06:52 -0600

    WaitForButton for Console

--- src/main/native/cpp/Utils/WaitForButton/WaitForButton.cpp
+++ src/main/native/cpp/Utils/WaitForButton/WaitForButton.cpp
@@ -5,3 +5,9 @@ frc2::CommandPtr WaitForButton(OverXboxController *gamepad, int buttonNumber) {
 		return gamepad->GetHID().GetRawButton(buttonNumber);
 	});
 }
+
+frc2::CommandPtr WaitForButton(frc2::CommandGenericHID *gamepad, int buttonNumber) {
+	return frc2::cmd::WaitUntil([=]() {
+		return gamepad->GetHID().GetRawButton(buttonNumber);
+	});
+}
