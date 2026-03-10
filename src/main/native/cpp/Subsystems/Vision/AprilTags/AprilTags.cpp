0ba7c91d5edf9aa900a49f7946c29e449b7d62b9
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2026-03-09 22:53:50 -0600

    Add Moving camera functionality (#63)
    * Moving camera
    
    * Use pose estimator camera to robot transform, instead of config
    
    * Spotless

--- src/main/native/cpp/Subsystems/Vision/AprilTags/AprilTags.cpp
+++ src/main/native/cpp/Subsystems/Vision/AprilTags/AprilTags.cpp
@@ -77,7 +77,8 @@ void AprilTags::addMeasurementToChassis(
 
 	for (const auto &t : result.GetTargets()) {
 		targets.push_back(
-				current3d.TransformBy(config.cameraToRobot).TransformBy(
+				current3d.TransformBy(
+						poseEstimator->GetRobotToCameraTransform()).TransformBy(
 						t.GetBestCameraToTarget()));
 	}
 	targetPosesPublisher.Set(targets);
@@ -93,13 +94,19 @@ void AprilTags::setEnabled(bool enabled) {
 	this->enabled = enabled;
 }
 
+std::optional<photon::EstimatedRobotPose> AprilTags::getEstimatedPose(
+		const photon::PhotonPipelineResult &result) {
+	auto visionEst = poseEstimator->EstimateCoprocMultiTagPose(result);
+	if (!visionEst) {
+		visionEst = poseEstimator->EstimateLowestAmbiguityPose(result);
+	}
+	return visionEst;
+}
+
 void AprilTags::Periodic() {
 	if (enabled) {
 		for (const auto &result : camera->GetAllUnreadResults()) {
-			auto visionEst = poseEstimator->EstimateCoprocMultiTagPose(result);
-			if (!visionEst) {
-				visionEst = poseEstimator->EstimateLowestAmbiguityPose(result);
-			}
+			auto visionEst = getEstimatedPose(result);
 			m_latestResult = result;
 
 			if (visionEst) {
