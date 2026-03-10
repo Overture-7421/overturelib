8008564dd12f5e201725438487d01859f2110492
Luis Leyva <luisoleyvan@gmail.com>
2026-03-09 23:53:08 -0600

    Refactor configureSwerveBase to accept PID constants as parameters

--- src/main/native/include/OvertureLib/Subsystems/Swerve/SwerveChassis/SwerveBase.h
+++ src/main/native/include/OvertureLib/Subsystems/Swerve/SwerveChassis/SwerveBase.h
@@ -32,7 +32,7 @@ public:
 	virtual frc::Rotation3d getRotation3d() = 0;
 
 protected:
-	void configureSwerveBase() {
+	void configureSwerveBase(PIDConstants translationPIDConstants, PIDConstants rotationPIDConstants) {
 
 		pathplanner::RobotConfig robotConfig = RobotConfig::fromGUISettings();
 
@@ -46,8 +46,8 @@ protected:
 			[this]() {return getCurrentSpeeds();},
 			[this](frc::ChassisSpeeds speeds) {setTargetSpeeds(speeds);},
 			std::make_shared<PPHolonomicDriveController>(
-				PIDConstants(6.5, 0.0, 0.0),
-				PIDConstants(6.5, 0.0, 0.0)
+				translationPIDConstants,
+				rotationPIDConstants
 			),
 			robotConfig,
 			[]() {
